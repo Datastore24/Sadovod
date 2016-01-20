bf1583829abdd619f753f673e128fa2fb5a17f1b
Viktor Mishustin <mrgyugo@gmail.com>
2016-01-20 20:11:40 +0300

    Работа с третьим вью

--- Sadovod/Sadovod/ViewControllerProductDetails.m
+++ Sadovod/Sadovod/ViewControllerProductDetails.m
@@ -7,25 +7,34 @@
 //
 
 #import "ViewControllerProductDetails.h"
+#import "TitleClass.h"
+#import "ViewProductDetails.h"
 
 @implementation ViewControllerProductDetails
 {
     UIScrollView * mainScrollView; //Основной скрол вью
 }
 
-- (void) viewWillAppear:(BOOL)animated
-{
-    self.title = @"Джампер";
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
+    TitleClass * title = [[TitleClass alloc]initWithTitle:@"Джампер"];
+    self.navigationItem.titleView = title;
+    
+    NSArray * array = [NSArray arrayWithObjects:@"1image.jpg", @"2image.jpg", @"3image.jpg",
+                                                @"5image.jpg", @"6image.jpg", nil];
     
     //Инициализация scrollView-----------
     mainScrollView = [[UIScrollView alloc] initWithFrame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     [self.view addSubview:mainScrollView];
     mainScrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height + 200);
+    
+    ViewProductDetails * scrollViewImge = [[ViewProductDetails alloc] initWithFrame:CGRectMake(0, 0,
+                                            self.view.frame.size.width,
+                                            self.view.frame.size.height / 3) andArray:array];
+    [mainScrollView addSubview:scrollViewImge];
 }
+                                           
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
