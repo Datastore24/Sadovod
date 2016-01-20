3cf63547a351a253def45893f048015577eec364
Viktor Mishustin <mrgyugo@gmail.com>
2016-01-20 20:29:51 +0300

    sdfr

---
+++ Sadovod/Sadovod/GuestPasswordViewController.m
@@ -0,0 +1,13 @@
+//
+//  GuestPasswordViewController.m
+//  Sadovod
+//
+//  Created by Viktor on 20.01.16.
+//  Copyright © 2016 Viktor. All rights reserved.
+//
+
+#import "GuestPasswordViewController.h"
+
+@implementation GuestPasswordViewController
+
+@end
