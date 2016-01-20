2c19a299e6dcf8761210a19615d2fb511ea4c1ca
Viktor Mishustin <mrgyugo@gmail.com>
2016-01-20 22:18:23 +0300

    добавленно вью изменения цены

--- Sadovod/Sadovod/ViewControllerProductDetails.m
+++ Sadovod/Sadovod/ViewControllerProductDetails.m
@@ -9,6 +9,7 @@
 #import "ViewControllerProductDetails.h"
 #import "TitleClass.h"
 #import "ViewProductDetails.h"
+#import "UIColor+HexColor.h"
 
 @implementation ViewControllerProductDetails
 {
@@ -25,6 +26,7 @@
     
     //Инициализация scrollView-----------
     mainScrollView = [[UIScrollView alloc] initWithFrame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    mainScrollView.backgroundColor = [UIColor colorWithHexString:@"f4f4f4"];
     [self.view addSubview:mainScrollView];
     mainScrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height + 200);
     
@@ -32,12 +34,40 @@
                                             self.view.frame.size.width,
                                             self.view.frame.size.height / 3) andArray:array];
     [mainScrollView addSubview:scrollViewImge];
+    
+    //Инициализация вью изменения цены-------------------------
+    UIView * priceView = [[UIView alloc] initWithFrame:CGRectMake(0, scrollViewImge.frame.size.height + 20,
+                                                                     self.view.frame.size.width, 40)];
+    priceView.backgroundColor = [UIColor colorWithHexString:@"3038a0"];
+    [mainScrollView addSubview:priceView];
+    //Лейбл цены------------------------------------------------
+    UILabel * priceLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 5, 60, 30)];
+    priceLabel.text = @"400 руб";
+    priceLabel.textColor = [UIColor whiteColor];
+    priceLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:16];
+    [priceView addSubview:priceLabel];
+    //Кнопка изменения цены-------------------------------------
+    UIButton *buttonChangePrice = [UIButton buttonWithType:UIButtonTypeCustom];
+    [buttonChangePrice addTarget:self
+               action:@selector(buttonChangePriceAction)
+     forControlEvents:UIControlEventTouchUpInside];
+    [buttonChangePrice setTitle:@"Изменить цену" forState:UIControlStateNormal];
+    [buttonChangePrice.titleLabel setFont:[UIFont systemFontOfSize:12]];
+    buttonChangePrice.frame = CGRectMake(self.view.frame.size.width - 100, 0, 100, 40.0);
+//    buttonChangePrice.backgroundColor = [UIColor whiteColor];
+    [priceView addSubview:buttonChangePrice];
 }
-                                           
+
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+
+- (void) buttonChangePriceAction
+{
+    NSLog(@"Изменить цену");
 }
 
 @end
