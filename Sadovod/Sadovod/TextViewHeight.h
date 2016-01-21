f36e630cab7f8751c394778e298b6443598253cf
Viktor Mishustin <mrgyugo@gmail.com>
2016-01-21 17:13:27 +0300

    Добавление класса высоты

---
+++ Sadovod/Sadovod/TextViewHeight.h
@@ -0,0 +1,13 @@
+//
+//  TextViewHeight.h
+//  Sadovod
+//
+//  Created by Viktor on 21.01.16.
+//  Copyright © 2016 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextViewHeight : UITextView
+
+@end
