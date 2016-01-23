4d9d83084c613ec30bfbf8bd25713d28cb4c936b
Viktor Mishustin <mrgyugo@gmail.com>
2016-01-23 17:42:04 +0300

    Работа с новыми вью

---
+++ Sadovod/Sadovod/CustomerViewController.m
@@ -0,0 +1,13 @@
+//
+//  CustomerViewController.m
+//  Sadovod
+//
+//  Created by Viktor on 23.01.16.
+//  Copyright © 2016 Viktor. All rights reserved.
+//
+
+#import "CustomerViewController.h"
+
+@implementation CustomerViewController
+
+@end
---
+++ Sadovod/Sadovod/ViewCustomer.h
@@ -0,0 +1,13 @@
+//
+//  ViewCustomer.h
+//  Sadovod
+//
+//  Created by Viktor on 23.01.16.
+//  Copyright © 2016 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewCustomer : UIView
+
+@end
